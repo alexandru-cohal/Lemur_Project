93b4b27975ec75a515141120606c19df74fcec38
alexandru-cohal <alexandru.cohal@yahoo.com>
2017-02-02 18:24:36 +0100

    Add files via upload
    Respond to "Are You Alive?" question from the Server by sending "I Am Alive" message

--- client.py
+++ client.py
@@ -5,8 +5,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("129.241.187.57", 20023))
 
 while True:
-	s.send("[Lemur] AYA?")
-	time.sleep(3)
+	data = s.recv(100)
+	
+	print "Received from Server: ", data
+
+	if data == "[Lemur] AYA?":
+		s.send("[Lemur] IAA")
 
 s.close()
-
