f991b61f6d5f004f1255186fea9020d6daf44d45
alexandru-cohal <alexandru.cohal@yahoo.com>
2017-02-23 18:39:59 +0100

    Add files via upload
    Watchdog Thread added.
    Receive Messages Thread added.
    Check if a component is dead (empty message or no message received).
    Update the connections and addresses lists when a component is dead.

--- component.py
+++ component.py
@@ -3,44 +3,120 @@ import time
 
 import network
 
-ip_elevator = ["129.241.187.153", "129.241.187.155", "129.241.187.151"]
-my_ip = network.Get_IP_Address()
+address_elevator = ["129.241.187.152", "129.241.187.155", "129.241.187.151"]
+my_address = network.Get_IP_Address()
 connection = []
 address = []
+flag_component_alive = []
 
 #---------------------------------------------------------------------------------------------------------
 def Listen_To_Components():
+	global connection
+	global address
+
 	print "I am listening for connections from components!"
 
-	s = network.Bind_Socket(my_ip, 22290)
+	s = network.Bind_Socket(my_address, 22290)
 
 	while True:
 		s.listen(1)
 
 		(conn, addr) = network.Accept_Connection(s)
+		addr = addr[0]		
 
 		connection.append(conn)		
-		address.append(addr[0])
+		address.append(addr)
+		flag_component_alive.append(1)
 
 		print "I am connected to ", len(connection), " components!" 
 		print "New Connected address:", address[-1]
 
+		thread.start_new_thread(Receive_From_Component,(conn, addr))
+		thread.start_new_thread(Watchdog_Component_Alive,(conn, addr))
+
 #---------------------------------------------------------------------------------------------------------
 def Connect_To_Components():
+	global connection
+	global address_elevator
+	global address
+
 	print "I am trying to connect to components!"
 
 	while True:
-		for ip in ip_elevator:
-			if (ip > my_ip) and (ip not in address): 
+		for addr in address_elevator:
+			if (addr > my_address) and (addr not in address): 
 				try:
-					connection.append( network.Create_Socket(ip, 22290) )
-					address.append(ip)
+					conn = network.Create_Socket(addr, 22290)		
+				except:
+					print "I did not manage to connect to ", addr
+				else:
+					connection.append(conn)
+					address.append(addr)
+					flag_component_alive.append(1)
 					print "New Connected address:", address[-1]
 					print "I am connected to ", len(connection), " components!" 
-				except:	
-					print "I did not manage to connect to ", ip
+
+					thread.start_new_thread(Receive_From_Component,(conn, addr))
+					thread.start_new_thread(Watchdog_Component_Alive,(conn, addr))
+
 			time.sleep(1)
 
+#---------------------------------------------------------------------------------------------------------
+def Eliminate_Component_From_Lists(addr):
+	global address
+	global connection
+
+	component_index = address.index(addr)
+
+	connection.pop(component_index)
+	address.pop(component_index)
+	flag_component_alive.pop(component_index)
+
+	print "I am now connected to ", len(connection), " components!"
+
+#---------------------------------------------------------------------------------------------------------
+def Watchdog_Component_Alive(conn, addr):
+	global address
+	global flag_component_alive
+
+	while True:
+		try:
+			component_index = address.index(addr)
+		except:
+			# Dead component (it was removed from the lists)
+			break
+		else:
+			if flag_component_alive[component_index] == 0:
+				# Dead component
+				print addr, "is dead (Network or Power lost)"
+				Eliminate_Component_From_Lists(addr)
+				break
+			else:
+				# Alive component
+				flag_component_alive[component_index] == 0
+				time.sleep(3)
+		
+#---------------------------------------------------------------------------------------------------------
+def Receive_From_Component(conn, addr):
+	global address
+	global flag_component_alive
+
+	print "I am receiving from ", addr
+
+	while True:
+		message = network.Receive_Message(conn)
+
+		if not message:
+			# Dead component
+			print addr, "is dead (Null message received)"
+			Eliminate_Component_From_Lists(addr)
+			break
+		else:
+			# Alive component
+			print "I received from ", addr, "the message: ", message
+			flag_component_alive[address.index(addr)] = 1
+			
+		
 #---------------------------------------------------------------------------------------------------------
 #def Watchdog_Client_Alive(conn, addr):
 
@@ -48,11 +124,13 @@ def Connect_To_Components():
 
 #add process pair
 #---------------------------------------------------------------------------------------------------------
-print 'I am ', my_ip
+print 'I am ', my_address
 
 thread.start_new_thread(Listen_To_Components,())
 thread.start_new_thread(Connect_To_Components,())
 
 while True:
-	time.sleep(1)
+	for conn in connection:
+		network.Send_Message(conn, "[Lemur] IAA")
 
+	time.sleep(2)
