c0c4ff50b8d72c30a98d322569120fb9a1d627f5
alexandru-cohal <alexandru.cohal@yahoo.com>
2017-03-03 16:02:44 +0100

    Add files via upload
    The initialization function of the elevator driver was moved on the first position in the Main function such that if the elevator is pressing a switch it should not continue (be dead)
    TO DO: When a component dies, reassign its unhandled order to someone else
    TO DO: When a component comes back, its queue should be synchronized with the others (by sending messages)
    TO TEST: Check if the connections are stable when a component comes back.

--- component.py
+++ component.py
@@ -4,7 +4,7 @@ import time
 import network
 import elev_driver
 
-address_elevator = ["129.241.187.153", "129.241.187.151", "129.241.187.157"]
+address_elevator = ["129.241.187.153", "129.241.187.157"]
 my_address = network.Get_IP_Address()
 connection = []
 address = []
@@ -88,6 +88,9 @@ def Watchdog_Component_Alive(conn, addr):
 	global flag_component_alive
 
 	while True:
+		# Tell the others that I am alive
+		network.Broadcast_Message(connection, "[Lemur] IAA")
+
 		try:
 			component_index = address.index(addr)
 		except:
@@ -103,7 +106,7 @@ def Watchdog_Component_Alive(conn, addr):
 				# Alive component
 				print addr, "is alive"
 				flag_component_alive[component_index] = 0
-				time.sleep(3)
+				time.sleep(1)
 		
 #---------------------------------------------------------------------------------------------------------
 def Receive_From_Component(conn, addr):
@@ -149,14 +152,12 @@ def Receive_From_Component(conn, addr):
 						if command_item[0] == int(message_items[2]) and command_item[1] == int(message_items[3]):
 							command_item[2] = message_items[4]
 					if my_address == message_items[4]:
-						thread.start_new_thread(Execute_Command, (int(message_items[3]), 0))
+						thread.start_new_thread(elev_driver.elev_driver_go_to_floor, (int(message_items[3]),))
 
 				if message_items[1] == "[Accomplishment]":
 					for command in commands_queue:
 						if command[2] == addr:
 							commands_queue.pop(commands_queue.index(command)) 
-							elev_driver.libelev.elev_set_button_lamp(0, command[1], 0)
-							elev_driver.libelev.elev_set_button_lamp(1, command[1], 0)
 			
 #---------------------------------------------------------------------------------------------------------
 def Assign_Component():
@@ -166,33 +167,30 @@ def Assign_Component():
 		# Check the commands queue
 		for command in commands_queue:
 			if command[2] == "":
-				print "Unhandled command found!"
-				flag_free_component = 0 # 0 = All components are Busy, 1 = At least one component is Free
-
-				while flag_free_component == 0:	
-					time.sleep(0.1)
-					if my_status == 0:
-						flag_free_component = 1
-						my_status = 1
-						command[2] = my_address
-						network.Broadcast_Message(connection, "[Lemur] " + "[Assignment] " + str(command[0]) + " " + str(command[1]) + " " + my_address)
-						thread.start_new_thread(Execute_Command, (command[1], 0))
-					else:
+				time.sleep(0.1)
+				# It is an unhandled command	
+				if my_status == 0:
+					my_status = 1
+					command[2] = my_address
+					network.Broadcast_Message(connection, "[Lemur] " + "[Assignment] " + str(command[0]) + " " + str(command[1]) + " " + my_address)				
+					thread.start_new_thread(elev_driver.elev_driver_go_to_floor, (command[1],))
+				else:
+					flag_free_component = 0 # 0 = All components are Busy, 1 = At least one component is Free
+					while flag_free_component == 0:
 						try:
 							free_component_index = component_status.index(0)
 						except:
 							pass
 						else:
 							flag_free_component = 1
-							component_status[free_component_index] = 1
-							command[2] = address[free_component_index]
-							network.Broadcast_Message(connection, "[Lemur] " + "[Assignment] " + str(command[0]) + " " + str(command[1]) + " " + address[free_component_index])
+					component_status[free_component_index] = 1
+					command[2] = address[free_component_index]
+					network.Broadcast_Message(connection, "[Lemur] " + "[Assignment] " + str(command[0]) + " " + str(command[1]) + " " + address[free_component_index])
 
 	flag_assign_component_thread_created = 0
 
 #---------------------------------------------------------------------------------------------------------
 def Execute_Command(floor, internal):
-	# internal = 1 if it is an internal command, 0 if it is an external command
 	global my_status
 	global commands_queue
 
@@ -210,9 +208,6 @@ def Execute_Command(floor, internal):
 		# Send a message to the others to pop out too from their queues
 		network.Broadcast_Message(connection, "[Lemur] " + "[Accomplishment]")
 
-		elev_driver.libelev.elev_set_button_lamp(0, floor, 0)
-		elev_driver.libelev.elev_set_button_lamp(1, floor, 0)
-
 	my_status = 0
 	network.Broadcast_Message(connection, "[Lemur] " + "[Status] " + "Free")
 
@@ -225,9 +220,6 @@ thread.start_new_thread(Connect_To_Components,())
 elev_driver.elev_driver_init()
 
 while True:
-	# Tell the others that I am alive
-	network.Broadcast_Message(connection, "[Lemur] [Alive]")
-
 	# Find the role in the network
 	if not address:
 		flag_master = 1
@@ -237,13 +229,22 @@ while True:
 		else:
 			flag_master = 0
 
+	# Announce the others whether I am BUSY or FREE
+	if elev_driver.elev_busy.acquire(False) == False:
+		my_status = 1
+		network.Broadcast_Message(connection, "[Lemur] " + "[Status] " + "Busy")
+	else:
+		my_status = 0
+		elev_driver.elev_busy.release()
+		network.Broadcast_Message(connection, "[Lemur] " + "[Status] " + "Free")
+
 	# Read the buttons of the elevator
 	(button, floor) = elev_driver.elev_driver_poll_buttons()
 	if button != -2 and floor != -2:
 		# A button was pressed
 		if button == 2:
 			# COMMAND button was pressed
-			thread.start_new_thread(Execute_Command, (floor, 1))
+			thread.start_new_thread(elev_driver.elev_driver_go_to_floor, (floor,))
 		else:
 			# UP or DOWN button was pressed
 			network.Broadcast_Message(connection, "[Lemur] " + "[Button] " + str(button) + " " + str(floor)), "\0"
@@ -256,10 +257,18 @@ while True:
 		if flag_assign_component_thread_created == 0:
 			flag_assign_component_thread_created = 1
 			thread.start_new_thread(Assign_Component,())
+		
+		#pass
 	else:
 		# Slave part
 		print "I am a Slave!"
 			
+
+		#master_conn = connection[ address.index( min(address) ) ]
+
+	#print "Others' Status: ", address, component_status
+	#print "My Status: ", my_status
+
 	print "Commands: ", commands_queue
 
 	time.sleep(0.5)
